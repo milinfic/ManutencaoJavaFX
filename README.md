eac31468cc4b666b8abb32ae61740bc942134e41
Paulo Cesar Pereira Alves <paulo.cpa@hotmail.com>
2021-10-11 12:03:21 -0300

    gui/mainView.fxml

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #ManutençãoJavaFX
+
+####inicio do projeto
+Efetuado a criação do projeto JavaFX com o pacote db que será responsável pela conexão com o Banco de Dados
+
+####Pacote gui/MainView.fxml
+Efetuado a criação da main fxml principal, onde será o palco inicial de todo projeto, e configuração no arquivo main para inicialização da view, dentro do aquivo main foi criado uma variável Scene static para pegar a mainView
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,31 +1,42 @@
 package application;
 	
-import java.io.IOException;
-import java.sql.SQLException;
-
-import db.DB;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+						
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Optiva System ** MANUTENÇÃO **");
 			primaryStage.show();
-		} catch(Exception e) {
+						
+			
+		}catch (Exception e) {
 			e.printStackTrace();
-		}
+		}		
 	}
 	
-	public static void main(String[] args) throws IOException, SQLException {
-		DB.getConnection();
+	public static void main(String[] args){
 		launch(args);
 	}
 }
